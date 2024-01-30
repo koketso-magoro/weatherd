f34f114dc6596098e7923572dd833ce24b1b856c
koketso-magoro <koketso.magoro06@gmail.com>
2024-01-30 15:07:19 +0200

    injected the forecast HTML into JavaScript

--- src/index.js
+++ src/index.js
@@ -51,8 +51,34 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-temperatures">
+      <span class="weather-forecast-temperature">
+        <strong>18°</strong>
+      </span>
+      <span class="weather-forecast-temperature-min">9°</span>
+    </div>
+  </div>
+`;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Bloemfontein");
+displayForecast();
